f5dc360c6b5abaa342000fd8a5edd21176b92f19
Moritz Lang <hi@slashmo.codes>
2020-02-06 20:29:14 +0100

    Initial Commit

---
+++ SwiftPM-Amplitude-Bug/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftPM-Amplitude-Bug
+//
+//  Created by Moritz Lang on 06.02.20.
+//  Copyright © 2020 Moritz Lang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
